efa3737e5768e62092ebce1310828659749f840e
KELVIN FERREIRA SOUZA <k.souza@gestor.local>
2020-03-12 15:12:26 -0300

    Desenvolvido a Model e a Migration de Schedule

---
+++ app/Models/Schedule.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Schedule extends Model
+{
+    protected $fillable = [
+        'doctor_id', 'date', 'busy'
+    ];
+
+    protected $table = 'schedules';
+}
---
+++ database/migrations/2020_03_07_171720_create_schedules_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSchedulesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('schedules', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('doctor_id')->unsigned()->onDelete('cascade');
+            $table->foreign('doctor_id')->references('id')->on('doctors')->onDelete('cascade');
+            $table->string('date', 112)->unique();
+            $table->string('busy', 112);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('schedules');
+    }
+}
